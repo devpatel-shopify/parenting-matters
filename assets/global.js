16b32bd2630750074489821709000507f597c889
Vishal Dev <53933007+devpatel-shopify@users.noreply.github.com>
2023-06-30 19:04:28 +0530

    feat: about page design
    about page design

--- assets/global.js
+++ assets/global.js
@@ -1223,5 +1223,27 @@ if (window.screen.width <= 767) {
 
 
 
+// Popup
+
+const modals = document.querySelectorAll('[data-modal]');
+
+modals.forEach(function (trigger) {
+  trigger.addEventListener('click', function (event) {
+    event.preventDefault();
+    const modal = document.getElementById(trigger.dataset.modal);
+    modal.classList.add('open');
+    document.body.classList.add("popup-open");
+    const exits = modal.querySelectorAll('.modal-exit');
+    exits.forEach(function (exit) {
+      exit.addEventListener('click', function (event) {
+        event.preventDefault();
+        modal.classList.remove('open');
+        document.body.classList.remove("popup-open");
+      });
+    });
+  });
+});
+
+
 
 
