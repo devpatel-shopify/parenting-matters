bd4f02e40b72dfed62afffb476b58c575bfe583b
Dev Xovac <dev.xovakstudio@gmail.com>
2023-11-08 14:32:37 +0530

    Course dashboard update

--- assets/dashboard-course.js
+++ assets/dashboard-course.js
@@ -1,13 +1,16 @@
 class dashboardCourse extends HTMLElement{
   constructor(){
     super();
+    this.sessionStorageContent = JSON.parse(window.sessionStorage.getItem('LastActiveCourse'));
     this.partsElement = this.querySelector('.js-parts-slider');
     this.stepSliderWrapper = this.querySelector('.js-step-sliders--wrapper');
     this.partsElement.querySelectorAll('.js-step-btn').forEach((button,index) => this.handleStepClick(button,index));
+    this.querySelectorAll('.js-course-btn').forEach((button) => this.handleCourseBtnClick(button))
     this._initPartsSlider();
   }
 
   _initPartsSlider(){
+    let _this = this;
     new Flickity(this.partsElement, {
       cellAlign: 'center',
       contain: true,
@@ -15,7 +18,8 @@ class dashboardCourse extends HTMLElement{
       prevNextButtons: false,
       on:{
         ready: function() {
-          let activeSlide = parseInt(this.element.dataset.activeSlide);
+          console.log(_this,_this.sessionStorageContent)
+          let activeSlide = (_this.sessionStorageContent) ? _this.sessionStorageContent.currentPartIndex : parseInt(this.element.dataset.activeSlide);
           if(activeSlide != 0){
             this.selectCell(activeSlide);
           }
@@ -23,6 +27,14 @@ class dashboardCourse extends HTMLElement{
         }
       }
     });
+  }
+
+  handleCourseBtnClick(button){
+    let slide = button.closest('.carousel-cell');
+    button.addEventListener('click',function(){
+      let localContent = JSON.parse(slide.querySelector('[type="application/json"]').innerText);
+      window.sessionStorage.setItem('LastActiveCourse',JSON.stringify(localContent));
+    })
   }
 
   handleStepClick(button,index){
@@ -37,6 +49,7 @@ class dashboardCourse extends HTMLElement{
   }
 
   _showSteps(currentStep){
+    let _this = this;
     let activeStep = this.stepSliderWrapper.querySelector('.js-steps-slider:not(.hidden)');
     if(activeStep){
       Flickity.data(activeStep).destroy();
@@ -56,7 +69,7 @@ class dashboardCourse extends HTMLElement{
       arrowShape: 'M92.5,42.5H25.6L37.8,30.3A7.5,7.5,0,1,0,27.2,19.7l-25,25a7.5,7.5,0,0,0,0,10.6l25,25a7.5,7.5,0,0,0,10.6,0,7.5,7.5,0,0,0,0-10.6L25.6,57.5H92.5a7.5,7.5,0,0,0,0-15Z',
       on:{
         ready:function(){
-          this.selectCell(parseInt(this.element.dataset.activeSlide));
+          this.selectCell((_this.sessionStorageContent) ? _this.sessionStorageContent.currentCourseIndex : parseInt(this.element.dataset.activeSlide));
         }
       }
     });
